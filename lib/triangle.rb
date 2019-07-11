a84ee9d4f9610c4ccc2a8be6a5e4740ff998916d
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-11 12:01:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,13 +17,13 @@ class Triangle
     else
         :isosceles #two equal sides
     end
-    # binding.pry
+    binding.pry
     if (side1 + side2) >= side3 || (side2 + side3) >= side1 || (side3 + side1) >= side2
         raise TriangleError #custom error
       # HINT1:The sum of the lengths of any two sides of a triangle always exceeds
       # the length of the third side. This is a principle known as the triangle inequality.
-    # if side1 <= 0 || side2 <= 0 || side3 <= 0
-    #     raise TriangleError #custom error
+    if side1 >= 0 || side2 >= 0 || side3 >= 0
+        raise TriangleError #custom error
       # each side must be larger than 0.
     end
 
