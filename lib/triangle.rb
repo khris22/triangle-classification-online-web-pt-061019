0e65474f9c48160bb38ce297c79469ba197bc266
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-11 12:14:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,17 +15,17 @@ class Triangle
         raise TriangleError #custom error
       # HINT1:The sum of the lengths of any two sides of a triangle always exceeds
       # the length of the third side. This is a principle known as the triangle inequality.
-      side1 >= 0 || side2 >= 0 || side3 >= 0
+    elsif side1 >= 0 || side2 >= 0 || side3 >= 0
         raise TriangleError #custom error
       # each side must be larger than 0.
-    end
-      if side1 == side2 && side2 == side3 && side1 == side3
+
+    elsif side1 == side2 && side2 == side3 && side1 == side3
           :equilateral #all sides equal
-      elsif side1 != side2 && side2 != side3 && side1 != side3
+    elsif side1 != side2 && side2 != side3 && side1 != side3
           :scalene # no equal sides
-      else
+    else
           :isosceles #two equal sides
-      end
+    end
     # end
   end
 
